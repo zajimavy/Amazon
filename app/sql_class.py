bd36aeae6e7cd02eb4fdb6fb2840f1bc620d671b
zajimavy <johnstockinger@gmail.com>
2018-03-07 10:38:43 -0700

    Update sql_class.py

--- app/sql_class.py
+++ app/sql_class.py
@@ -4,14 +4,14 @@ import os
 
 class SQL:
 	def upload(self, query):
-		connection_str = """ Driver={SQL Server Native Client 11.0}; Server=FNZBISQL01\DAX_SQL_BI; Database=dw; Trusted_Connection=yes; """
+		connection_str = """Connection STRING"""
 		cnxn = pyodbc.connect(connection_str)
 		cnxn.autocommit = True
 		cursor = cnxn.cursor()
 		cursor.execute(query)
 
 	def runQuery(self, query):
-		connection_str = """ Driver={SQL Server Native Client 11.0}; Server=FNZBISQL01\DAX_SQL_BI; Database=dw; Trusted_Connection=yes; """
+		connection_str = """ Connection STRING """
 		cnxn = pyodbc.connect(connection_str)
 		cnxn.autocommit = True
 		cursor = cnxn.cursor()
@@ -20,7 +20,7 @@ class SQL:
 		return results
 		
 	def executeScriptsFromFile(self,filename):
-		connection_str = """ Driver={SQL Server Native Client 11.0}; Server=FNZBISQL01\DAX_SQL_BI; Database=dw; Trusted_Connection=yes; """
+		connection_str = """Connection STRING """
 		cnxn = pyodbc.connect(connection_str)
 		cnxn.autocommit = True
 		cursor = cnxn.cursor()
@@ -36,7 +36,7 @@ class SQL:
 		return results
 
 	def executeScriptsFromFileNoResults(self,filename):
-		connection_str = """ Driver={SQL Server Native Client 11.0}; Server=FNZBISQL01\DAX_SQL_BI; Database=dw; Trusted_Connection=yes; """
+		connection_str = """ Connection STRING """
 		cnxn = pyodbc.connect(connection_str)
 		cnxn.autocommit = True
 		cursor = cnxn.cursor()
@@ -50,7 +50,7 @@ class SQL:
 
 class Teradata:
 	def runQuery(self, query):
-		connection_str = """ Driver={Teradata}; dbcname=141.206.21.20; dsn = vm; authentication = TD2; uid={%s}; pwd={$tdwallet(pass_win)};""" % os.getenv('username')
+		connection_str = """ Connection STRING""" % os.getenv('username')
 		cnxn = pyodbc.connect(connection_str, autocommit=True)
 		cnxn.autocommit = True
 		cursor = cnxn.cursor()
@@ -59,7 +59,7 @@ class Teradata:
 		return results
 		
 	def upload(self, query):
-		connection_str = """ Driver={Teradata}; dbcname=141.206.21.20; dsn = vm; authentication = TD2; uid={%s}; pwd={$tdwallet(pass_win)};""" % os.getenv('username')
+		connection_str = """ Connection STRING""" % os.getenv('username')
 		cnxn = pyodbc.connect(connection_str, autocommit=True)
 		cnxn.autocommit = True
 		cursor = cnxn.cursor()
@@ -69,7 +69,7 @@ class Teradata:
 	
 	            
 if __name__ == "__main__": #this is what runs at the cmd line
-	 result = executeScriptsFromFile("N:\Planning\John\SQL Queries\Python Queries\\sql_test_files.sql")
+	 result = executeScriptsFromFile("FILE PATH\\sql_test_files.sql")
 	 print type(result)
 	 print result
 	 
